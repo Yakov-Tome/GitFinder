1bb7bddcdb3178108728782c88f6fb8548bf0565
yakovtome@outlook.com <yakovtome@gmail.com>
2025-02-17 21:37:55 +0200

    Pages & Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen bg-neutral/10">
-        <Navbar />
+        <Navbar title="GitFinder" />
 
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
