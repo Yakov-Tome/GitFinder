aa0a2eb01626aacb4a4cb9d5b9a69c98da27c6e6
yakovtome@outlook.com <yakovtome@gmail.com>
2025-02-17 21:07:37 +0200

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
